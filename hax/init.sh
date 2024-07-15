61a14a29cae669bd2994ee26fc55ecc4d691e007
Royan Xu <132798457+happylabdab2@users.noreply.github.com>
2024-07-15 22:01:45 +0000

    inproved the script

--- hax/init.sh
+++ hax/init.sh
@@ -1,6 +1,18 @@
+#/bin/bash
+
+#Check for sudo
+if [ `id -u` -ne 0 ]
+  then echo Please run this script as root or using sudo!
+  exit
+fi
+
+#Replace Ping with Altered ping
 sudo mv /bin/ping /bin/ping1
-sudo xz -d -k ./ping.xz
+sudo xz -dfk ./ping.xz
 sudo mv ./ping /bin/ping
 sudo chmod +x /bin/ping
 echo $? -eq
 echo "Done"
+
+#Instructions
+echo "Poweroff ,log back in, turn off your wifi, press ctrl+alt+t, run ping, select option 0"
